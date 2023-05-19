e3d7389f65d8a9aec3a94a9efd2c840d5af66216
skII22 <1240301433@qq.com>
2023-05-19 17:25:20 +0800

    '第一次提交'

---
+++ 前端/vue-admin-template/src/views/changePassword/changePw.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main">
+    <el-form ref="form" label-width="80px">
+      <el-form-item class="form">
+        <el-form-item label="原密码">
+          <el-input v-model="oldPw" class="input" style="width: 200px"></el-input>
+        </el-form-item>
+        <el-form-item label="新密码" >
+          <el-input v-model="newPw" style="width: 200px"></el-input>
+        </el-form-item>
+        <div class="message" v-if="passwordMatch === false" style="color: red">两次密码不一致,请重新查看后输入</div>
+        <el-form-item label="确认密码" >
+          <el-input v-model="confirmPw" style="width: 200px"></el-input>
+        </el-form-item>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" :disabled="passwordMatch === false" @click="submit(oldPw,newPw)" style="margin-left: 140px">提交</el-button>
+  </div>
+</template>
+
+<script src="./changePassword.js">
+export default {
+  name: 'changePw'
+}
+</script>
+<style scoped="scoped">
+.main{
+text-align: center;
+  margin-top: 300px;
+}
+
+</style>
+
+
